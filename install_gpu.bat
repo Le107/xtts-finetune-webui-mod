159c5007fc893a22fe3c12208ef15823866796ac
Le107 <157118445+Le107@users.noreply.github.com>
2025-02-14 00:25:49 +0300

    Update install_gpu.bat

--- install_gpu.bat
+++ install_gpu.bat
@@ -3,7 +3,7 @@ call venv/scripts/activate
 
 .\venv\Scripts\python.exe -m pip install --upgrade pip
 pip uninstall torch torchaudio
-pip3 install torch torchaudio --index-url https://download.pytorch.org/whl/cu118
+pip3 install torch==2.5.1 torchaudio==2.5.1 --index-url https://download.pytorch.org/whl/cu118
 pip install -r .\requirements.txt
 
 cmd
