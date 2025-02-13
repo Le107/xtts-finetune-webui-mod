4a63ff3674547130d58e83a343c0024ae5544758
Le107 <157118445+Le107@users.noreply.github.com>
2025-02-14 00:26:01 +0300

    Update install_cpu.bat

--- install_cpu.bat
+++ install_cpu.bat
@@ -3,7 +3,7 @@ call venv/scripts/activate
 
 .\venv\Scripts\python.exe -m pip install --upgrade pip
 pip uninstall torch torchaudio
-pip install torch torchaudio
+pip install torch==2.5.1 torchaudio==2.5.1
 pip install -r .\requirements.txt
 
 cmd
